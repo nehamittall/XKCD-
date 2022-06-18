6fba90f0ab5067163271b88790ede38098fd6cce
nxhacxde <98503932+nxhacxde@users.noreply.github.com>
2022-06-18 23:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
-#! python3
+import bs4, requests, os
 
-# downloadXkcd.py - Downloads every single XKCD comic.
+os.makedirs('xkcd', exist_ok = True)
 
-import requests, os, bs4
-url = 'http://xkcd.com' 		# starting url
-os.makedirs('xkcd',exist_ok = True)		# create a directory to store all the downloads
+url = 'http://xkcd.com'
 
-while not url.endswith("#"):
-	# Download the page.
-	print("Downloading the page ... ")
-	res = requests.get(url)
-	res.raise_for_status()
-	try:
-		soup = bs4.BeautifulSoup(res.text,'lxml')
-	except bs4.FeatureNotFound:  # lxml is not installed
-		soup = bs4.BeautifulSoup(res.text,'html.parser')
+while not url.endswith('#'):
+    print('Downloading page ' + url)
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    
+    try:
+        comicURL = 'http:' + soup.select('#comic img')[0].get('src')
+        print('Downloading image ' + comicURL)
+        res = requests.get(comicURL)
+        imageFile = open(os.path.join('xkcd', os.path.basename(comicURL)), 'wb')
+        for chunk in res:
+            imageFile.write(chunk)
+        imageFile.close()
+    except Exception as exc:
+        print('Could not find image due to ' + str(exc))
 
-	# Find the URL of the comic image.
-	comic_element = soup.select('#comic img')
-	if comic_element == []:
-		print("No comic image found!!..")
-	else:
-		comic_image_url = comic_element[0].get('src')
-		# download the image
-		print("Downloading the image %s .. " %(comic_image_url))
-		res = requests.get('http:' + comic_image_url)
-		res.raise_for_status()
-
-		# Save the image to ./xkcd.
-		file = open( os.path.join('xkcd',os.path.basename(comic_image_url)) , 'wb')
-		for chunk in res.iter_content(10000):
-			file.write(chunk)
-		file.close()
-
-	# Get the Prev button's url.
-	prev_link = soup.select('a[rel="prev"]')[0]
-	url = 'http://xkcd.com' + prev_link.get('href')
-
-print("Done")
+    url = 'http://xkcd.com' + soup.select('a[rel="prev"]')[0].get('href')
